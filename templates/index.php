df275f2160bf62df8eb2c2082d993b8632abc9d2
SoveC <59285975+SoveC@users.noreply.github.com>
2021-03-15 10:51:36 +0300

    Add files via upload

---
+++ templates/index.php
@@ -0,0 +1,45 @@
+
+    <section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+
+            <!--заполните этот список из массива категорий-->
+            <?php foreach ($categories as $item): ?>
+                <li class="promo__item promo__item--boards">
+                    <a class="promo__link" href="pages/all-lots.html"><?=$item;?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+
+            <!--заполните этот список из массива с товарами-->
+
+            <?php foreach ($data_list as $lot): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$lot['gif'];?>" width="350" height="260" alt="">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$lot['category'];?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['name'];?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=format_sum($lot['price']);?></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                 <?=time_left($times_left)?>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
+
